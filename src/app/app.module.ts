87fa98f9fb3bc2cb07f8fb3bfb460f3dfd51e28e
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-12 20:08:25 +0100

    added a bar graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { D3Service } from 'd3-ng2-service';
 
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { D3graphComponent } from './d3graph/d3graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    D3graphComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [D3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
